c680d959666e0a98658fef6d4a2ff6497d9be242
Nisarg Patel <74370955+nisargpatel1504@users.noreply.github.com>
2024-01-25 23:20:26 -0500

    Update joinList.js
    I have written the function that basically  joins the contents of conceptList together
     * into one String, concepts, with each list item separated from
     * the previous by a comma.

--- joinList.js
+++ joinList.js
@@ -13,6 +13,17 @@
 // ...
 
 // Test / Driver Code below...
+
+function joinList(list) {
+  if (list.length === 0) {
+    return '';
+  }
+  let result = list[0];
+  for (let i = 1; i < list.length; i++) {
+    result += `, ${list[i]}`;
+  }
+  return result;
+}
 const conceptList = ["gists", "types", "operators", "iteration", "problem solving"];
 const concepts = joinList(conceptList);
 console.log(`Today I learned about ${concepts}.`);
